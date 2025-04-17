8d3d5909e1c05f4e0ce914077330003d5af025a5
Alaukik Patel <alaukikpatel232@gmail.com>
2025-04-17 09:35:43 +0530

    fix3

--- app/creator/[creatorId]/page.tsx
+++ app/creator/[creatorId]/page.tsx
@@ -1,6 +1,12 @@
 import React from 'react';
 
-export default function CreatorPage({ params }: { params: { creatorId: string } }) {
+interface PageProps {
+  params: {
+    creatorId: string;
+  };
+}
+
+export default function CreatorPage({ params }: PageProps) {
   return (
     <div>
       <h1>Creator ID: {params.creatorId}</h1>
